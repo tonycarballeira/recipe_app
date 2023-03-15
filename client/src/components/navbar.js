32288d7f467189e63186262c26587415fb0aae59
tonycarballeira <tonycarballeira@gmail.com>
2023-03-15 13:20:47 -0400

    setup login form to talk to api and created logout button

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,23 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {useCookies} from 'react-cookie';
 
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userID");
+        navigate("/auth");
+    };
+
     return <div className="navbar">
         <Link to="/"> Home</Link>
         <Link to="/create-recipe"> Create Recipe</Link>
         <Link to="/saved-recipes"> Saved Recipes</Link>
-        <Link to="/auth"> Login/ register</Link>
+        {!cookies.access_token ? (<Link to="/auth"> Login/ register</Link>) : <button onClick={logout}>Logout</button>}
+        
     </div>
 }
 
--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from 'axios';
+import {useCookies, usecookies} from 'react-cookie';
+import {useNavigate} from 'react-router-dom';
 
 export const Auth = () => {
     return <div className="auth">
@@ -13,7 +15,24 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    return <Form label="Login" username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", {username, password});
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+            navigate("/");
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    return <Form onSubmit={onSubmit} label="Login" username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
     
 };
 
