22e43f68099646b116e8b133d3e399d26ccb7ff9
tonycarballeira <tonycarballeira@gmail.com>
2023-03-16 19:08:52 -0400

    created route for recipes created by a user

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -39,4 +39,26 @@ router.put("/", async (req, res) => {
     }
 });
 
+router.get("/savedRecipes/ids", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID);
+        res.json({savedRecipes: user?.savedRecipes});
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.get("/savedRecipes", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID);
+        const savedRecipes = await RecipeModel.find({
+            _id: { $in: user.savedRecipes },
+        }); 
+
+        res.json({savedRecipes});
+    } catch (err) {
+        res.json(err);
+    }
+});
+
 export { router as recipesRouter};
